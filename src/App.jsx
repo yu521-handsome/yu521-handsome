9f83ccdb8f9dc0ddf7b6dfdcea58830378d6e6bf
gcctbilly <gcctxhl@163.com>
2022-01-23 19:31:12 +0800

    clear file to prepare new version

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React, { Component } from 'react'
 import {Route,Switch,Redirect} from 'react-router-dom'
 
-import industryPageRoute from './page/industryPage/industryPageRoute'
-import ProfessorPageRoute from './page/professorPage/ProfessorPageRoute'
-import StudentPageRoute from './page/studentPage/StudentPageRoute'
+
 
 export default class App extends Component {
     render() {
         return(
             <div>
                 <Switch>
-                <Route path="/industryPage" component={industryPageRoute}/>
-                <Route path="/professor" component= {ProfessorPageRoute}/>
-                <Route path="/student" component= {StudentPageRoute}/>
-                <Redirect to="/professor"/>
                 </Switch>
             </div>
         )
