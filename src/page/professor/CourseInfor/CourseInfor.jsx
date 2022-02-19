361ac83b1128a6339de09f871d64bf1f64380731
gcctbilly <gcctxhl@163.com>
2022-02-19 15:59:10 +0800

    Clear unnecessary file. rename SetAccount

--- src/page/professor/CourseInfor/CourseInfor.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class CourseInfor extends Component {
-  render() {
-    return <div>Need Implement</div>;
-  }
-}
--- src/page/professor/CourseSet/CourseSet.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class CourseSet extends Component {
-  render() {
-    return <div>Need Implement</div>;
-  }
-}
